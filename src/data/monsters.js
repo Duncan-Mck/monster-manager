9bec9bab9a0997c984a8866b63c6ae77843adeae
Duncan-Mck <duncanmckirdy@gmail.com>
2016-10-20 19:30:42 -0400

    Updated monster.js
    Added the iconic beholder

--- src/data/monsters.js
+++ src/data/monsters.js
@@ -3245,6 +3245,112 @@ const monsters = [
     ]
   },
   {
+    "name": "Beholder",
+    "size": "Large",
+    "type": "aberration",
+    "subtype": "",
+    "alignment": "lawful evil",
+    "armor_class": 18,
+    "hit_points": 180,
+    "hit_dice": "19d10",
+    "speed": "0 ft., fly 20 ft.",
+    "strength": 10,
+    "dexterity": 14,
+    "constitution": 18,
+    "intelligence": 17,
+    "wisdom": 15,
+    "charisma": 17,
+    "perception": 12,
+    "damage_vulnerabilities": "",
+    "damage_resistances": "",
+    "damage_immunities": "lightning",
+    "condition_immunities": "prone",
+    "senses": "darkvision 90 ft., passive Perception 16",
+    "languages": "Deep Speech",
+    "challenge_rating": 13,
+    "special_abilities": [
+      {
+        "name": "Antimagic Cone",
+        "desc": "The beholder's central eye creates an area of antimagic, as in the anti magic field spell, in a 150-foot cone. At the start of each of its turns, the beholder decides which way the cone faces and whether the cone is active. The area works against the beholder's own eye rays.",
+        "attack_bonus": 0
+      }
+    ],
+    "actions": [
+      {
+        "name": "Bite",
+        "desc": "Melee Weapon Attack: +5 to hit, reach 5 ft., one target. Hit: 14 (4d6) piercing damage.",
+        "attack_bonus": 10,
+        "damage_dice": "3d10",
+        "damage_bonus": 6
+      },
+      {
+        "name": "Eye Rays",
+        "desc": " The beholder shoots three of the following magical eye rays at random (reroll duplicates), choosing one to three targets it can see within 120 feet of it.",
+        "attack_bonus": 0
+      },
+      {
+        "name": "Charm Ray",
+        "desc": "The targeted creature must succeed on a DC 16 Wisdom saving throw or be charmed by the beholder for 1 hour, or until the beholder harms the creature.",
+        "attack_bonus": 0
+      },
+      {
+        "name": "Paralyzing Ray",
+        "desc": " The targeted creature must succeed on a DC 16 Constitution saving throw or be paralyzed for 1 minute. The target can repeat the saving throw at the end of each of its turns, ending the effect on itself on a success.",
+        "attack_bonus": 0
+      },
+      {
+        "name": "Fear Ray",
+        "desc": "The targeted creature must succeed on a DC 16 Wisdom saving throw or be frightened for 1 minute. The target can repeat the saving throw at the end of each of its turns, ending the effect on itself on a success.",
+        "attack_bonus": 0
+      },
+      {
+        "name": "Slowing Ray",
+        "desc": " The targeted creature must succeed on a DC 16 Dexterity saving throw. On a failed save, the target's speed is halved for 1 minute. In addition, the creature can't take reactions, and it can take either an action or a bonus action on its turn, not both. The creature can repeat the saving throw at the end of each of its turns, ending the effect on itself on a success.",
+        "attack_bonus": 0
+      },
+      {
+        "name": "Enervation Ray",
+        "desc": "The targeted creature must make a DC 16 Constitution saving throw, taking 36 (8d8) necrotic damage on a failed save, or half as much damage on a successful one",
+        "attack_bonus": 0,
+        "damage_dice": "8d8"
+      },
+      {
+        "name": "Telekinetic Ray",
+        "desc": "If the target is a creature, it must succeed on a DC 16 Strength saving throw or the beholder moves it up to 30 feet in any direction. It is restrained by the ray's telekinetic grip until the start of the beholder's next turn or until the beholder is incapacitated. If the target is an object weighing 300 pounds or less that isn't being worn or carried, it is moved up to 30 feet in any direction. The beholder can also exert fine control on objects with this ray, such as manipulating a simple tool or opening a door or a container.",
+        "attack_bonus": 0
+      },
+      {
+        "name": "Sleep Ray",
+        "desc": "The targeted creature must succeed on a DC 16 Wisdom saving throw or fall asleep and remain unconscious for 1 minute. The target awakens if it takes damage or another creature takes an action to wake it. This ray has no effect on constructs and undead.",
+        "attack_bonus": 0
+      },
+      {
+        "name": "Petrification Ray",
+        "desc": "The targeted creature must make a DC 16 Dexterity saving throw. On a failed save, the creature begins to turn to stone and is restrained. It must repeat the saving throw at the end of its next turn. On a success, the effect ends. On a failure, the creature is petrified until freed by the greater rest.oration spell or other magic.",
+        "attack_bonus": 0
+      },
+      {
+        "name": "Disintegration Ray",
+        "desc": "If the target is a creature, it must succeed on a DC 16 Dexterity saving throw or take 45 (10d8) force damage. If this damage reduces the creature to 0 hit points, its body becomes a pile of fine gray dust. If the target is a Large or smaller non magical object or creation of magical force, it is disintegrated without a saving throw. If the target is a Huge or larger object or creation of magical force, this ray disintegrates a 10-foot cube of it.",
+        "attack_bonus": 0,
+        "damage_dice": "10d8"
+      },
+      {
+        "name": "Death Ray",
+        "desc": "The targeted creature must succeed on a DC 16 Dexterity saving throw or take 55 (10d10) necrotic damage. The target dies if the ray reduces it to 0 hit points.",
+        "attack_bonus": 0,
+        "damage_dice": "10d10"
+      }
+    ],
+    "legendary_actions": [
+      {
+        "name": "Eye Ray",
+        "desc": "The beholder uses one random eye ray.",
+        "attack_bonus": 0
+      }
+    ]
+  },
+  {
     "name": "Berserker",
     "size": "Medium",
     "type": "humanoid",
